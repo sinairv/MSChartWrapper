23168f1244dab3b1ee416383f7920cd08a05ca21
Sina Iravanian <sina.iravanian@gmail.com>
2011-12-31 17:23:34 +0330

    Solved Issue #6: Now the Chart form methods receive owner

--- MSChartWrapper.UI/FormMain.cs
+++ MSChartWrapper.UI/FormMain.cs
@@ -52,7 +52,7 @@ namespace MSChartWrapper.UI
                 cosine[i] = Math.Cos(i * 0.01);
             }
 
-            ChartForm.ShowLineChartForm(
+            ChartForm.ShowLineChartForm(this,
                 new[] { "Sine", "Cosine" }, new[] { sine, cosine }, 
                 "Sine vs Cosine", "x", "value", "Line Chart Window Demo");
         }
@@ -63,7 +63,7 @@ namespace MSChartWrapper.UI
             int[] vals2 = new[] { 7, 5, 3, 2, 1 };
             string[] labels = new [] {"year 1", "year 2", "year 3", "year 4", "year 5"};
 
-            ChartForm.ShowBarChartForm(new[] { "Company 1", "Company 2" }, labels,
+            ChartForm.ShowBarChartForm(this, new[] { "Company 1", "Company 2" }, labels,
                 new[] { vals1, vals2 }, "Performance of Companies", 
                 "year", "income in billion dollars", "Bar Chart Window Demo");
         }
--- MSChartWrapper/ChartForm.cs
+++ MSChartWrapper/ChartForm.cs
@@ -19,13 +19,14 @@ namespace MSChartWrapper
         /// <summary>
         /// Shows a chart form in a separate window with line charts plotted
         /// </summary>
+        /// <param name="owner">The top-level window that will own this form</param>
         /// <param name="names">The name of each line to be plotted</param>
         /// <param name="values">The values of each line to be plotted. One array per line.</param>
         /// <param name="chartTitle">The title of the chart</param>
         /// <param name="xTitle">The title of the x-axis</param>
         /// <param name="yTitle">The title of the y-axis</param>
         /// <param name="formTitle">The title of the chart form</param>
-        public static void ShowLineChartForm(string[] names, double[][] values, string chartTitle, string xTitle, string yTitle, string formTitle)
+        public static void ShowLineChartForm(IWin32Window owner, string[] names, double[][] values, string chartTitle, string xTitle, string yTitle, string formTitle)
         {
             var frm = new ChartForm();
 
@@ -39,7 +40,56 @@ namespace MSChartWrapper
             frm.chartMain.AxisYTitle = yTitle;
             frm.Text = formTitle;
 
-            frm.Show();
+            if (owner != null)
+                frm.Show(owner);
+            else
+                frm.Show();
+        }
+
+        /// <summary>
+        /// Shows a chart form in a separate window with line charts plotted
+        /// </summary>
+        /// <param name="names">The name of each line to be plotted</param>
+        /// <param name="values">The values of each line to be plotted. One array per line.</param>
+        /// <param name="chartTitle">The title of the chart</param>
+        /// <param name="xTitle">The title of the x-axis</param>
+        /// <param name="yTitle">The title of the y-axis</param>
+        /// <param name="formTitle">The title of the chart form</param>
+        public static void ShowLineChartForm(string[] names, double[][] values, string chartTitle, string xTitle, string yTitle, string formTitle)
+        {
+            ShowLineChartForm(null, names, values, chartTitle, xTitle, yTitle, formTitle);
+        }
+
+        /// <summary>
+        /// Shows a chart form in a separate window with bar charts plotted
+        /// </summary>
+        /// <typeparam name="T">the type of values to be plotted</typeparam>
+        /// <param name="owner">The top-level window that will own this form</param>
+        /// <param name="names">The name of each bar-chart to be plotted</param>
+        /// <param name="labels">The labels (per value, not series)</param>
+        /// <param name="values">The values of each line to be plotted. One array per line.</param>
+        /// <param name="chartTitle">The title of the chart</param>
+        /// <param name="xTitle">The title of the x-axis</param>
+        /// <param name="yTitle">The title of the y-axis</param>
+        /// <param name="formTitle">The title of the chart form</param>
+        public static void ShowBarChartForm<T>(IWin32Window owner, string[] names, string[] labels, T[][] values, string chartTitle, string xTitle, string yTitle, string formTitle)
+        {
+            var frm = new ChartForm();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                frm.chartMain.AddBarPlot(names[i], labels, values[i]);
+            }
+
+            frm.chartMain.Title = chartTitle;
+            frm.chartMain.AxisXTitle = xTitle;
+            frm.chartMain.AxisYTitle = yTitle;
+            frm.Text = formTitle;
+
+            if (owner != null)
+                frm.Show(owner);
+            else
+                frm.Show();
         }
 
         /// <summary>
@@ -55,19 +105,7 @@ namespace MSChartWrapper
         /// <param name="formTitle">The title of the chart form</param>
         public static void ShowBarChartForm<T>(string[] names, string[] labels, T[][] values, string chartTitle, string xTitle, string yTitle, string formTitle)
         {
-            var frm = new ChartForm();
-
-            for (int i = 0; i < names.Length; i++)
-            {
-                frm.chartMain.AddBarPlot(names[i], labels, values[i]);
-            }
-
-            frm.chartMain.Title = chartTitle;
-            frm.chartMain.AxisXTitle = xTitle;
-            frm.chartMain.AxisYTitle = yTitle;
-            frm.Text = formTitle;
-
-            frm.Show();
+            ShowBarChartForm(null, names, labels, values, chartTitle, xTitle, yTitle, formTitle);
         }
     }
 }
